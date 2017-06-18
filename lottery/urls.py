ca0edfc9483a36fd06ffaa752c580764cde6758e
bazinga12 <yekotovskiy@gmail.com>
2017-06-18 18:04:42 +0300

    5

--- lottery/urls.py
+++ lottery/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.stati import static
+from django.conf.urls.static import static
 from game.views import home_view
 from . import settings
 
