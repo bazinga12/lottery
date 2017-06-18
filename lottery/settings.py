9ca7e0a342ecc5ee0c7fa1b0979f6c1c685bc72f
bazinga12 <yekotovskiy@gmail.com>
2017-06-18 17:50:02 +0300

    1

--- lottery/settings.py
+++ lottery/settings.py
@@ -126,11 +126,4 @@ DATABASES['default'].update(db_from_env)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
